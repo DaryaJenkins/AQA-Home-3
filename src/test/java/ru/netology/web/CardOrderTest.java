d020df586810507addd3ad1784b52fd0e60ab162
Darya Jenkins <melissegreenleaf@gmail.com>
2025-02-07 19:04:31 +0300

    fixes

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -50,77 +50,77 @@ class CardOrderTest {
     @Test
     void sendInvalidNameInLatin() {
         fillForm("Daria Ivanenko", "+79031110022", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"));
         Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.getText().trim());
     }
 
     @Test
     void sendInvalidNameWithSymbols() {
         fillForm("Дарья! Ив@ненко", "+79031110022", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"));
         Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.getText().trim());
     }
 
     @Test
     void sendOnlySurname() {
         fillForm("Дарья", "+79031110022", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"));
         Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.getText().trim());
     }
 
     @Test
     void sendFormWithoutName() {
         fillForm("", "+79031110022", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"));
         Assertions.assertEquals("Поле обязательно для заполнения", result.getText().trim());
     }
 
     @Test
     void sendInvalidPhoneWithoutPlus() {
         fillForm("Дарья Иваненко", "79031110022", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
     }
 
     @Test
     void sendInvalidPhoneWithTenChars() {
         fillForm("Дарья Иваненко", "+7903110022", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
     }
 
     @Test
     void sendInvalidPhoneWithTwelveChars() {
         fillForm("Дарья Иваненко", "+790311002222", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
     }
 
     @Test
     void sendInvalidPhoneWithOneChar() {
         fillForm("Дарья Иваненко", "1", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
     }
 
     @Test
     void sendInvalidPhoneWithTwoChars() {
         fillForm("Дарья Иваненко", "12", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
     }
 
     @Test
     void sendInvalidPhoneWithLetters() {
         fillForm("Дарья Иваненко", "+790311100AB", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.getText().trim());
     }
 
     @Test
     void sendFormWithoutPhone() {
         fillForm("Дарья Иваненко", "", true);
-        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'] .input__sub"));
+        WebElement result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"));
         Assertions.assertEquals("Поле обязательно для заполнения", result.getText().trim());
     }
 
